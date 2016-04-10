796d6a3af415ab224db79987e4a39e722f84b397
mariko33 <maria.t.georgieva2012@gmail.com>
2016-04-10 08:50:28 +0300

    add

---
+++ Problem2-StudentPage/scripts/myController.js
@@ -0,0 +1,10 @@
+app.controller('studentInfo',[$scope, function($scope){
+    $scope.info={
+        "name": "Pesho",
+        "photo": "http://www.nakov.com/wp-content/uploads/2014/05/SoftUni-Logo.png",
+        "grade": 5,
+        "school": "High School of Mathematics",
+        "teacher": "Gichka Pesheva"
+
+    }
+}])
---
+++ Problem3-BindImageSourceToTextBox/scripts/controller.js
@@ -0,0 +1,4 @@
+app.controller('imgController',function($scope){
+var imgUrl;
+    $scope.imgUrl=imgUrl;
+})
